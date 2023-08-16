b4ce4a107235eb641f097124abd4bfff40072988
Przemysław Kitowski <kitowski.przemek@gmail.com>
2023-08-16 20:39:14 +0200

    Styles box && playboard components

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,8 +1,13 @@
 import styles from './Box.module.css'
 
+const playersStyles = {
+    X: styles.red,
+    O: styles.green,
+}
+
 const Box = ({ value, handleClick }) => {
     return (
-        <button className={styles.box} onClick={handleClick}>
+        <button className={`${styles.box} ${(value ? playersStyles[value] : '')}`} onClick={handleClick}>
             {value}
         </button>
     )
